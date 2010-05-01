8c818cfb16b10008cfb692448f7d6904ad95d9b3
Zhang Yuanyi <zhangyuanyi@Zhang-Yuanyis-MacBook.local>
2010-05-01 10:04:29 +0800

    增加exception notification

---
+++ config/initializers/mail_setting.rb
@@ -0,0 +1,11 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "51qiangzuo.com",
+  :port                 => 25,
+  :domain               => "51qiangzuo.com",
+  :user_name            => "noreply@51qiangzuo.com",
+  :password             => "tellmewhy",
+  :authentication       => "login",
+  :enable_starttls_auto => true
+} 
+
+ActionMailer::Base.default_url_options[:host] = (Rails.env == 'production' ? "51qiangzuo.com" : "localhost:3001")
