2abad524dc50ab165490749c76ffd84614e0bfd8
Zhang Yuanyi <zhangyuanyi@Zhang-Yuanyis-MacBook.local>
2010-05-01 10:04:29 +0800

    增加exception notification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
 
-  # render new.rhtml
   def new
   end
 
--- app/observers/user_observer.rb
+++
@@ -1,9 +0,0 @@
-class UserObserver < ActiveRecord::Observer
-  def after_create(user)
-    Mailer.signup_notification(user).deliver
-  end
-
-  def after_save(user)
-    Mailer.activation(user).deliver if user.recently_activated?
-  end
-end
