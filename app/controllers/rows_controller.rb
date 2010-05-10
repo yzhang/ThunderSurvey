e41f146197a051794e9a1bdb995dd6389caf13de
Zhang Yuanyi <zhangyuanyi@Zhang-Yuanyis-MacBook.local>
2010-05-11 04:13:20 +0800

    fix form submit

--- app/controllers/rows_controller.rb
+++ app/controllers/rows_controller.rb
@@ -53,8 +53,11 @@ class RowsController < ApplicationController
     respond_to do |want|
       if @form.allow_insert? && @row.save
         @res = @form.deliver_notification(@row)
-       # want.html {redirect_to thanks_form_path(@form,:embed => params[:embed],:res => @res)}   
-        want.js { render :js => "parent.window.location='#{@res}'" }
+        want.js do
+          render :update do |page|
+            page << "window.location='#{thanks_form_url(@form, :embed => true)}'"
+          end
+        end
       else
         @embed = params[:embed]
         @order_id = @row.order_id
