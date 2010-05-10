806746ef467379a5d3119696f57ebf32c69b0ef3
Zhang Yuanyi <zhangyuanyi@Zhang-Yuanyis-MacBook.local>
2010-05-11 04:45:07 +0800

    修复异常通知

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,3 +1,3 @@
 Confforge::Application.config.middleware.use ExceptionNotifier, :email_prefix => "[表单异常] ",
    :sender_address => %{"Noreply" <noreply@51qiangzuo.com>},
-   :exception_recipients => %w{zhangyuanyi@gmail.com wear63659220@gmail.com} if Rails.root == 'production'
+   :exception_recipients => %w{zhangyuanyi@gmail.com wear63659220@gmail.com} if Rails.env == 'production'
