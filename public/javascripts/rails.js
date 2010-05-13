dcde179090d91c28c5024fae80d58bd3a76422b2
wear <wear63659220@gmail.com>
2010-05-13 13:25:27 +0800

    update rails.js

--- public/javascripts/rails.js
+++ public/javascripts/rails.js
@@ -12,7 +12,7 @@ jQuery(function ($) {
          *       own events and placing ourselves at the end of the chain.
          */
         triggerAndReturn: function (name, data) {
-            var event = new jQuery.Event(name);
+            var event = new $.Event(name);
             this.trigger(event, data);
 
             return event.result !== false;
@@ -27,15 +27,16 @@ jQuery(function ($) {
                 method  = el.attr('method') || el.attr('data-method') || 'GET',
                 url     = el.attr('action') || el.attr('href');
 
-            // TODO: should let the developer know no url was found
-            if (url !== undefined) {
+            if (url === undefined) {
+              throw "No URL specified for remote call (action or href must be present).";
+            } else {
                 if (el.triggerAndReturn('ajax:before')) {
                     $.ajax({
                         url: url,
                         data: data,
+                        dataType: 'script',
                         type: method.toUpperCase(),
                         beforeSend: function (xhr) {
-                            xhr.setRequestHeader("Accept", "text/javascript");
                             el.trigger('ajax:loading', xhr);
                         },
                         success: function (data, status, xhr) {
@@ -71,50 +72,55 @@ jQuery(function ($) {
     /**
      * remote handlers
      */
-    $('form[data-remote="true"]').live('submit', function (e) {
+    $('form[data-remote]').live('submit', function (e) {
         $(this).callRemote();
         e.preventDefault();
     });
 
-    $('a[data-remote="true"],input[data-remote="true"]').live('click', function (e) {
+    $('a[data-remote],input[data-remote]').live('click', function (e) {
         $(this).callRemote();
         e.preventDefault();
     });
 
-    $('a[data-method][data-remote!=true]').live('click',function(e){
+    $('a[data-method]:not([data-remote])').live('click', function (e){
         var link = $(this),
             href = link.attr('href'),
             method = link.attr('data-method'),
-            form = $('<form method="post" action="'+href+'">'),
-            input = $('<input name="_method" value="'+method+'" type="hidden" />'),
-            csrf_input = $('<input name="'+csrf_param+'" value="'+csrf_token+'" type="hidden" />');
+            form = $('<form method="post" action="'+href+'"></form>'),
+            metadata_input = '<input name="_method" value="'+method+'" type="hidden" />';
+
+        if (csrf_param != null && csrf_token != null) {
+          metadata_input += '<input name="'+csrf_param+'" value="'+csrf_token+'" type="hidden" />';
+        }
 
         form.hide()
-            .append(input)
-            .append(csrf_input)
-            .appendTo('body'); // redundant?
+            .append(metadata_input)
+            .appendTo('body');
 
         e.preventDefault();
         form.submit();
     });
 
     /**
-     * disable_with handlers
+     * disable-with handlers
      */
-    $('form[data-remote="true"]').live('ajax:before', function () {
-        $(this).children('input[data-disable-with]').each(function () {
+    var disable_with_input_selector = 'input[data-disable-with]';
+    var disable_with_form_selector = 'form[data-remote]:has(' + disable_with_input_selector + ')';
+
+    $(disable_with_form_selector).live('ajax:before', function () {
+        $(this).find(disable_with_input_selector).each(function () {
             var input = $(this);
-            input.data('enable_with', input.val())
+            input.data('enable-with', input.val())
                  .attr('value', input.attr('data-disable-with'))
                  .attr('disabled', 'disabled');
         });
     });
 
-    $('form[data-remote="true"]').live('ajax:after', function () {
-        $(this).children('input[data-disable-with]').each(function () {
+    $(disable_with_form_selector).live('ajax:after', function () {
+        $(this).find(disable_with_input_selector).each(function () {
             var input = $(this);
             input.removeAttr('disabled')
-                 .val(input.data('enable_with'));
+                 .val(input.data('enable-with'));
         });
     });
 });
