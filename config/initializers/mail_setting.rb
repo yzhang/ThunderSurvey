6b176756dae429ebeb83a21cbeb4ab425a54d6f9
Zhang Yuanyi <zhangyuanyi@Zhang-Yuanyis-MacBook.local>
2010-06-01 21:07:46 +0800

    update mail setting

--- config/initializers/mail_setting.rb
+++ config/initializers/mail_setting.rb
@@ -1,11 +1,11 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "51qiangzuo.com",
+  :address              => "51qiangda.com",
   :port                 => 25,
-  :domain               => "51qiangzuo.com",
-  :user_name            => "noreply@51qiangzuo.com",
+  :domain               => "51qiangda.com",
+  :user_name            => "noreply@51qiangda.com",
   :password             => "tellmewhy",
   :authentication       => "login",
   :enable_starttls_auto => true
 } 
 
-ActionMailer::Base.default_url_options[:host] = (Rails.env == 'production' ? "www.51qiangzuo.com" : "localhost:3001")
+ActionMailer::Base.default_url_options[:host] = (Rails.env == 'production' ? "www.51qiangda.com" : "localhost:3000")
