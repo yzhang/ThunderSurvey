dfa08a17df44bd6631918dfc02c28d254f637b61
Zhang Yuanyi <zhangyuanyi@Zhang-Yuanyis-MacBook.local>
2010-06-03 14:41:46 +0800

    更新结果展示

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,3 +11,19 @@ $(document).ready(function(){
 	});
 	$("input[type='checkbox']").css({'border':'0 none'})   
 });
+
+function fetch_row(form_id, row_id,key)
+{
+	$("#row").html($("#spinner").html());
+  $.get('/forms/'+form_id+'/rows/'+row_id, {edit_key : key}, function(data){
+    $("#row").html(data);
+  });
+}
+
+function edit_row(form_id,row_id,key)
+{
+	$("#row").html($("#spinner").html());
+  $.get('/forms/'+form_id+'/rows/'+row_id+'/edit', {edit_key : key}, function(data){
+    $("#row").html(data);
+  });
+}
