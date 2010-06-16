76d4202374f95ef38769b0e188ed41034307e06a
Zhang Yuanyi <zhangyuanyi@Zhang-Yuanyis-MacBook.local>
2010-06-16 15:56:27 +0800

    为用户自动创建帐号

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,14 @@ class HomeController < ApplicationController
     @section = 'home'
     redirect_to forms_url if logged_in?
   end
+  
+  def demo
+    user = User.new(:login => "临时用户#{Time.now.to_i}")
+    user.save(:validate => false)
+    self.current_user = user
+    
+    respond_to do |wants|
+      wants.html {redirect_to forms_url}
+    end
+  end
 end
